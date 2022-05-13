8ec9b0150e8304ad76a014f6c99c231a25775f30
Felipe <fjourdanp@gmail.com>
2022-05-13 18:25:11 +0100

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import { Header } from '../components/Header'
+
+export default function Home() {
+  return (
+    <>
+      <Head>TodoList :)</Head>
+      <Header />
+      <main>
+        <h1>Hello, World!</h1>
+      </main>
+    </>
+  )
+}
